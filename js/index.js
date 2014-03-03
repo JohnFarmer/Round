7337485730e6bb6d9ad8bbdc43073e2c713ef7d2
zhfuzzy <zhfuzzy@gmail.com>
2014-03-03 09:48:14 +0800

    full upload

---
+++ js/index.js
@@ -0,0 +1,44 @@
+var peerColorScheme = {};
+var peerName = 'Lucky' + Math.floor(Math.random() * 1000);
+var roomInfo;
+
+var roomlist = document.getElementById('roomlist');
+var sendBtn = document.getElementById('sendbtn');
+
+peerColorScheme['init'] = "#ABCDEF";
+peerColorScheme['wheel'] = mkColorWheel(peerColorScheme['init']);
+
+handlePageColor();
+
+var socket = io.connect();
+
+socket.emit('onboard home page', peerName);
+
+socket.on('room info', function(roominfo) {
+    console.log('socket on room info');
+    roomInfo = roominfo;
+    handleRoomList();
+});
+
+var handleRoomList = function() {
+    console.log('print room list:');
+    for (var key in roomInfo) {
+	var listItem = document.createElement('p');
+	listItem.innerHTML = 
+	    '<a href="/room/\?' + key + '">' + key + '</a>   ' + (roomInfo >= 4? 'Full': roomInfo[key]);
+	roomlist.appendChild(listItem);
+	console.log(key);
+    }
+};
+
+sendBtn.onclick = function() {
+    var targetRoomName = document.getElementById('roomnamebox').value;
+    if (targetRoomName == '') {
+	console.log('Empty');
+	document.getElementById('roomnamebox').value = '';
+	alert('Box can\'t be empty~');
+	return;
+    }
+    
+    window.location.replace('/room/?' + targetRoomName);
+};
